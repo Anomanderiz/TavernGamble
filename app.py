f473b6ad35c22a41ae5d1e79d61131d497527d1c
Anomanderiz <91763965+Anomanderiz@users.noreply.github.com>
2025-11-25 16:03:15 -0800

    Update app.py

--- app.py
+++ app.py
@@ -290,8 +290,8 @@ app_ui = ui.page_fluid(
               position: absolute;
               top: 50%;
               left: 50%;
-              width: 118%;
-              height: 118%;
+              width: 250%;
+              height: 250%;
               max-width: none;
               max-height: none;
               object-fit: contain;
